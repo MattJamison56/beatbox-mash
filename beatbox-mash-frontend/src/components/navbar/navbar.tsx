5afa070833b80551e0d987bb1798b0fcf69f1d1e
Matthew Jamison <matthewjamison56@gmail.com>
2024-06-15 14:40:32 -0700

    navbarstyledish

--- beatbox-mash-frontend/src/components/navbar/navbar.tsx
+++ beatbox-mash-frontend/src/components/navbar/navbar.tsx
@@ -1,14 +1,15 @@
 import React, { useState, MouseEvent } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
 import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import logo from '../../assets/beatboxlogo.png';
 import './navbar.css';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { Button } from '@mui/material';
 
 const Navbar: React.FC = () => {
   const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
@@ -41,61 +42,72 @@ const Navbar: React.FC = () => {
         anchor="top"
         open={drawerOpen}
         onClose={handleDrawerClose}
-        PaperProps={{ style: { width: '100%', top: 64, position: 'absolute', zIndex: 1200 } }}
+        PaperProps={{ style: { width: 'auto', top: 64, position: 'absolute', zIndex: 1200 } }}
       >
-        <Box p={2}>
+        <Box p={3} display="flex" justifyContent="flex-start" flexWrap="wrap" marginTop='20px' marginLeft='20px' marginBottom='20px'>
           {submenu === 'planEvents' && (
-            <List>
-              <ListItem button>
-                <ListItemText primary="View Events" />
-              </ListItem>
-              <ListItem button>
-                <ListItemText primary="Create Single Event" />
-              </ListItem>
-              <ListItem button>
-                <ListItemText primary="Create Multiple Events" />
-              </ListItem>
-            </List>
+            <>
+              <Box mr={2} sx={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', borderRight: '1px solid gray', paddingRight: '10px' }}>
+                <Typography variant="h6" sx={{ display: 'inline-flex', alignItems: 'center', padding: '10px', whiteSpace: 'nowrap', paddingRight: '50px', color: '#6f65ac'}}>
+                  <EventNoteIcon sx={{ marginRight: '8px' }} />
+                  View Events
+                </Typography>
+                <button className="subcat" style={{ width: '100%' }}>List View</button>
+                <button className="subcat" style={{ width: '100%' }}>Calendar View</button>
+                <button className="subcat" style={{ width: '100%' }}>Map View</button>
+                <button className="subcat" style={{ width: '100%' }}>Check In Status</button>
+              </Box>
+              <Box mr={2} sx={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', borderRight: '1px solid gray', paddingRight: '10px' }}>
+                <Typography variant="h6" sx={{ display: 'inline-flex', alignItems: 'center', padding: '10px', whiteSpace: 'nowrap', paddingRight: '50px', color: '#6f65ac'}}>
+                  <CalendarTodayIcon sx={{ marginRight: '8px' }} />
+                  Create Single Event
+                </Typography>
+                <button className="subcat" style={{ width: '100%' }}>By Date or Range</button>
+                <button className="subcat" style={{ width: '100%' }}>By Calendar</button>
+                <button className="subcat" style={{ width: '100%' }}>By Map by Venue</button>
+                <button className="subcat" style={{ width: '100%' }}>By Map by BA</button>
+                <button className="subcat" style={{ width: '100%' }}>By Multi Shift</button>
+              </Box>
+              <Box sx={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <Typography variant="h6" sx={{ display: 'inline-flex', alignItems: 'center', padding: '10px', whiteSpace: 'nowrap', paddingRight: '50px', color: '#6f65ac'}}>
+                  <CalendarMonthIcon sx={{ marginRight: '8px' }} />
+                  Create Multiple Events
+                </Typography>
+                <button className="subcat" style={{ width: '100%' }}>By Specific Date</button>
+                <button className="subcat" style={{ width: '100%' }}>By Date-range</button>
+                <button className="subcat" style={{ width: '100%' }}>By Calendar</button>
+                <button className="subcat" style={{ width: '100%' }}>Import Scheduled Events</button>
+                <button className="subcat" style={{ width: '100%' }}>Import Completed Events</button>
+              </Box>
+            </>
           )}
           {submenu === 'approvePay' && (
-            <List>
-              <ListItem button>
-                <ListItemText primary="Approve Invoices" />
-              </ListItem>
-              <ListItem button>
-                <ListItemText primary="Pay Bills" />
-              </ListItem>
-            </List>
+            <Box mr={2} sx={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', borderRight: '1px solid gray', paddingRight: '10px' }}>
+              <Typography variant="h6" sx={{ display: 'inline-flex', alignItems: 'center', padding: '0 8px', whiteSpace: 'nowrap' }}>Approve Invoices</Typography>
+              <button className="subcat" style={{ width: '100%' }}>Approve Invoices</button>
+              <button className="subcat" style={{ width: '100%' }}>Pay Bills</button>
+            </Box>
           )}
           {submenu === 'reportAnalyze' && (
-            <List>
-              <ListItem button>
-                <ListItemText primary="Generate Reports" />
-              </ListItem>
-              <ListItem button>
-                <ListItemText primary="Analyze Data" />
-              </ListItem>
-            </List>
+            <Box mr={2} sx={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', borderRight: '1px solid gray', paddingRight: '10px' }}>
+              <Typography variant="h6" sx={{ display: 'inline-flex', alignItems: 'center', padding: '0 8px', whiteSpace: 'nowrap' }}>Report & Analyze</Typography>
+              <button className="subcat" style={{ width: '100%' }}>Generate Reports</button>
+              <button className="subcat" style={{ width: '100%' }}>Analyze Data</button>
+            </Box>
           )}
           {submenu === 'setUp' && (
-            <List>
-              <ListItem button>
-                <ListItemText primary="User Settings" />
-              </ListItem>
-              <ListItem button>
-                <ListItemText primary="Preferences" />
-              </ListItem>
-            </List>
+            <Box mr={2} sx={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', borderRight: '1px solid gray', paddingRight: '10px' }}>
+              <Typography variant="h6" sx={{ display: 'inline-flex', alignItems: 'center', padding: '0 8px', whiteSpace: 'nowrap' }}>Set Up</Typography>
+              <button className="subcat" style={{ width: '100%' }}>User Settings</button>
+              <button className="subcat" style={{ width: '100%' }}>Preferences</button>
+            </Box>
           )}
           {submenu === 'admin' && (
-            <List>
-              <ListItem button>
-                <ListItemText primary="Manage Users" />
-              </ListItem>
-              <ListItem button>
-                <ListItemText primary="System Settings" />
-              </ListItem>
-            </List>
+            <Box sx={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <Typography variant="h6" sx={{ display: 'inline-flex', alignItems: 'center', padding: '0 8px', whiteSpace: 'nowrap' }}>Admin</Typography>
+              <button className="subcat" style={{ width: '100%' }}>Manage Users</button>
+              <button className="subcat" style={{ width: '100%' }}>System Settings</button>
+            </Box>
           )}
         </Box>
       </Drawer>
