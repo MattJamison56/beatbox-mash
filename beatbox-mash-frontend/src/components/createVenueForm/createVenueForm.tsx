64b04d9a5d8b357798238bbcf57da2492683b0ab
Matthew Jamison <matthewjamison56@gmail.com>
2024-08-28 21:56:43 -0700

    com5

--- beatbox-mash-frontend/src/components/createVenueForm/createVenueForm.tsx
+++ beatbox-mash-frontend/src/components/createVenueForm/createVenueForm.tsx
@@ -4,6 +4,7 @@ import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, A
 import { GoogleMap, LoadScriptNext, Libraries, MarkerF } from '@react-google-maps/api';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 const libraries: Libraries = ['places'] as const;
 
 type Venue = {
